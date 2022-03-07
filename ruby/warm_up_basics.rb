f064afae2c7ed5da4a50927982644841f1180c6e
Cat <cathy.moran@teladochealth.com>
2022-03-06 19:47:41 -0600

    warm up problem adding elements in array

---
+++ ruby/warm_up_basics.rb
@@ -0,0 +1,8 @@
+#sum of all elements in array
+
+def aVeryBigSum(ar)
+    sum = 0
+    ar.each {|num| sum+=num}
+    sum
+end
+#----------------------------------------------------------------------
