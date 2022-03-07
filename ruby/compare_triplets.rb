6a212f8cb7841d2fdf13e79ce6548c6503803396
Cat <cathy.moran@teladochealth.com>
2022-03-06 19:48:33 -0600

    compare elements in array

---
+++ ruby/compare_triplets.rb
@@ -0,0 +1,20 @@
+# Compare a[0] with b[0], a[1] with b[1], and a[2] with b[2]. If a[i] > b[i], then Alice is awarded 1 point. 
+# If a[i] < b[i], then Bob is awarded 1 point. If a[i] = b[i], then neither person receives a point. 
+# Comparison points is the total points a person earned. Given a and b, determine their respective comparison points. 
+
+def compareTriplets(a, b)
+    arr = [0,0]
+    i = 0
+
+    while i < a.length
+        arr[0]+=1 if a[i] > b[i]
+        arr[1]+=1 if a[i] < b[i]
+        i+=1
+    end
+    arr
+end
+
+list_a = [1, 2, 3]
+list_b = [3, 2, 1]
+
+p compareTriplets(list_a, list_b)
