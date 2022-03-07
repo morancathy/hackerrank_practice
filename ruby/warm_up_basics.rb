646f8aa1945f8067b68856ef9758ec6ce5c05368
Cat <cathy.moran@teladochealth.com>
2022-03-07 09:38:20 -0600

    serial_avg warm up

--- ruby/warm_up_basics.rb
+++ ruby/warm_up_basics.rb
@@ -1,8 +1,23 @@
 #sum of all elements in array
-
 def aVeryBigSum(ar)
     sum = 0
     ar.each {|num| sum+=num}
     sum
 end
-#----------------------------------------------------------------------
+  #--------------------------------------------
+# code a serial_average method which is described below:
+# It takes a fixed width string in format: SSS-XX.XX-YY.YY. SSS is a three digit serial number, XX.XX and YY.YY are two digit numbers including up to two decimal digits.
+# It returns a string containing the answer in format SSS-ZZ.ZZ where SSS is the serial number of that input string, and ZZ.ZZ is the average of XX.XX and YY.YY.
+# All numbers are rounded off to two decimal places.
+def serial_average(num)
+    serial = num[0,3]
+    first_dig = num[4,5].to_f
+    second_dig = num[10, 5].to_f
+  
+    avg = ((first_dig + second_dig)/2).round(2)
+  
+    output = "#{serial}-#{avg}"
+   end
+  
+  p serial_average('001-12.43-56.78')
+  #--------------------------------------------
