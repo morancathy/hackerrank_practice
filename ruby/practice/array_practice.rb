6f646cd05e344975a1599f7bbbcf1e9528030de4
Cat <cathy.moran@teladochealth.com>
2022-03-08 17:41:19 -0600

    array practice

--- ruby/practice/array_practice.rb
+++ ruby/practice/array_practice.rb
@@ -4,4 +4,19 @@ words = ["level", "selfless", "racecar","cathy"]
 palindrome = words.select { |word| word == word.reverse}
 p palindrome
 
+#---------------------------------------------------
+#return array of odds and evens from an array. On one line, seperate by coma.
+arr = [1, 2 , 3, 4, 5, 6]
 
+def evens_and_odds(numbers)
+    even = []
+    odd = []
+    numbers.each { |num| num.odd? ? odd << num : even << num}
+ end
+ 
+ def evens_and_odds1(numbers)
+    numbers.partition{ |num| num.odd? }
+ end
+ evens_and_odds(arr)
+ p evens_and_odds1(arr)
+ #---------------------------------------------------
