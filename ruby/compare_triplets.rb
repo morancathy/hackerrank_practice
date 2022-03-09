bdfbc7787f4f454c57d33d8ed8c0bb4d4b3940b9
Cat <cathy.moran@teladochealth.com>
2022-03-08 19:17:05 -0600

    min-mix of an array

--- ruby/compare_triplets.rb
+++ ruby/compare_triplets.rb
@@ -5,7 +5,6 @@
 def compareTriplets(a, b)
     arr = [0,0]
     i = 0
-
     while i < a.length
         arr[0]+=1 if a[i] > b[i]
         arr[1]+=1 if a[i] < b[i]
@@ -18,3 +17,18 @@ list_a = [1, 2, 3]
 list_b = [3, 2, 1]
 
 p compareTriplets(list_a, list_b)
+  #--------------------------------------------
+#refactored
+def compareTriplets1(a, b)
+    arr = [0,0]
+    a.each.with_index do |num, index|
+        arr[0]+=1 if num > b[index]
+        arr[1]+=1 if num < b[index]
+    end
+    arr
+end
+
+list_c = [4, 3, 1]
+list_d = [3, 2, 1]
+
+p compareTriplets1(list_c, list_d)
---
+++ ruby/min_max_array.rb
@@ -0,0 +1,17 @@
+#Given five positive integers, find the minimum and maximum values 
+#that can be calculated by summing exactly four of the five integers. 
+#Print the respective minimum and maximum values as a single line 
+#of two space-separated long integers.
+def miniMaxSum(arr)    
+    def min(array) 
+        min = array.sort[0...-1]
+        min.inject(:+)        
+    endq
+    def max(array) 
+        max = array.sort.reverse[0...-1]
+        max.inject(:+)    
+    end  
+    print min(arr), " ", max(arr) 
+end
+
+miniMaxSum([7, 9, 5, 1, 3])
