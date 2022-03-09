b56cd35e155dd767531dd5a73e57d346767fb0f2
Cat <cathy.moran@teladochealth.com>
2022-03-09 11:55:53 -0600

    ratio

---
+++ ruby/ratio.rb
@@ -0,0 +1,21 @@
+#Given an array of integers, calculate the ratios of its elements
+# that are zero, negative, and positive. Print the decimal value 
+# of each fraction on a new line.
+def plusMinus(arr)
+    ratio_array = [0,0,0]
+
+    arr.each do |num|
+        if num == 0
+            ratio_array[0] +=1
+        elsif num < 0
+            ratio_array[1] +=1
+        elsif
+            ratio_array[2] +=1
+        end       
+    end
+    ratio_array.map{|ele| p ele.to_f/arr.length}
+end
+
+plusMinus([1,2,0,-1,-1])
+plusMinus([0, 0, 5, -9, -9, 0, -1, -1])
+
